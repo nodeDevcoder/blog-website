3deaa5cea0ad14d8265c696cb6aed322dc47d805
nodeDevcoder <jibrilasif@gmail.com>
2021-07-06 12:02:16 -0700

    initial commit

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const app = express();
 
+const routes = require("./routes");
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.use((req, res, next) => {
+    next();
+});
+
+app.use(routes);
 
 app.listen(3000, () => {
     console.log(`Server is listening on port 3000`);
